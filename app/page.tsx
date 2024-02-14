3985083ad1661979aa87169e220c5c978db8ebf1
Aleksei Gorshkov <aleksei.gorshkov@adevinta.com>
2024-02-14 10:37:43 +0100

    multiroller

--- app/page.tsx
+++ app/page.tsx
@@ -7,31 +7,56 @@ enum State {
   ROLLED,
 }
 
+class Die {
+  constructor(dice = 6, roll = rand(6)) {
+    this.dice = dice;
+    this.currentRoll = roll;
+  }
+
+  dice: number;
+  currentRoll: number;
+
+  roll() {
+    let newRoll = rand(this.dice);
+    while (newRoll == this.currentRoll) {
+      newRoll = rand(this.dice);
+    }
+    this.currentRoll = newRoll;
+  }
+}
+
+function rand(n: number) {
+  return Math.floor(Math.random() * n);
+}
+
 export default function Home() {
   const emojis = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
 
   const [state, setState] = useState(State.IDLE);
-  const [roll, setRoll] = useState<number>(0);
-
-  function getRoll() {
-    return Math.floor(Math.random() * emojis.length);
-  }
+  const [dice, setDice] = useState([new Die()]);
 
   function handleRollClick() {
     setState(State.ROLLING);
     let previousRoll = -1;
     const timer = setInterval(() => {
-      let roll = getRoll();
-      while (previousRoll === roll) {
-        roll = getRoll();
-      }
-      setRoll(roll);
+      dice.forEach((die) => {
+        die.roll();
+      });
+      setDice([...dice]);
     }, 100);
 
     setTimeout(() => {
       clearInterval(timer);
       setState(State.ROLLED);
     }, 1000);
+  }
+
+  function handleRemoveDie() {
+    setDice(dice.slice(0, -1));
+  }
+
+  function handleAddDie() {
+    setDice([...dice, new Die()]);
   }
 
   return (
@@ -44,6 +69,20 @@ export default function Home() {
         >
           {state === State.ROLLING ? "Rolling..." : "Roll the dice!"}
         </button>
+        <button
+          disabled={dice.length === 1 || state === State.ROLLING}
+          className="bg-amber-200 rounded p-2 disabled:bg-slate-200"
+          onClick={handleRemoveDie}
+        >
+          Remove dice
+        </button>
+        <button
+          disabled={state === State.ROLLING}
+          className="bg-amber-200 rounded p-2 disabled:bg-slate-200"
+          onClick={handleAddDie}
+        >
+          Add dice
+        </button>
         {state === State.IDLE && (
           <ul className="flex gap-1 text-9xl">
             {emojis.map((e) => (
@@ -51,16 +90,21 @@ export default function Home() {
             ))}
           </ul>
         )}
-        {state !== State.IDLE && (
-          <div
-            className={
-              state === State.ROLLING
-                ? "text-8xl text-yellow-500"
-                : "text-9xl text-green-500"
-            }
-          >
-            {emojis[roll]}
-          </div>
+
+        {dice.map(
+          (die, i) =>
+            state !== State.IDLE && (
+              <div
+                key={i}
+                className={
+                  state === State.ROLLING
+                    ? "text-9xl text-yellow-500"
+                    : "text-9xl text-green-500"
+                }
+              >
+                {emojis[die.currentRoll]}
+              </div>
+            ),
         )}
       </div>
     </main>
